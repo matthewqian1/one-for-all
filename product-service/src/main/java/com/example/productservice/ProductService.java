1d60c68e4ec264a54d53264faacab365bea84434
Matthew <matthew.qian2@gmail.com>
2023-12-04 20:12:30 +1100

    add product list ot data base connectivity

--- product-service/src/main/java/com/example/productservice/ProductService.java
+++ product-service/src/main/java/com/example/productservice/ProductService.java
@@ -1,18 +1,21 @@
 package com.example.productservice;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,6 +25,12 @@ public class ProductService {
 
     ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
 
+    @PostConstruct
+    public void load() throws IOException {
+        InputStream stream = this.getClass().getClassLoader().getResourceAsStream("data.json");
+        List<Product> productList = mapper.readValue(stream, new TypeReference<List<Product>>() {});
+        productList.forEach(p -> products.put(generateId(), p));
+    }
 
     public void addProduct(Product product) {
         try {
@@ -35,7 +44,13 @@ public class ProductService {
     }
 
     public List<Product> getAllProducts() {
-        return products.values().stream().toList();
+        List<Product> list = products.values().stream().toList();
+        int multiplier = (int) Math.ceil((double) 10 / list.size());
+        List<Product> newList = new ArrayList<>();
+        for (int i = 0; i < multiplier; i++) {
+            newList.addAll(list);
+        }
+        return newList;
     }
 
     public List<Product> getProductsByCategory(Category category) {
